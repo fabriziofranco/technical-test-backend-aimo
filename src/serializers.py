193b499d3246e2607fb9a938de2bcfa1d294bcf5
fabriziofranco <fabrizio.franco@utec.edu.pe>
2022-08-25 11:49:50 -0500

    Backend completed

--- src/serializers.py
+++ src/serializers.py
@@ -14,16 +14,33 @@ class UserSerializer(Schema):
         user = User.select().where(User.username==value)
         if len(user) >0:
             raise ValidationError("Username is already in use")
-
+ 
 
 class NoteSerializer(Schema):
     id = fields.Integer()
     title = fields.String(required=True)
     creation_date = fields.Date()
-    
+
+    @validates("title")
+    def title_validations(self, value):
+        if len(value) > 200:
+            raise ValidationError("Title must be at most 20 characters")    
+
+
+class LoginSerializer(Schema):
+    id = fields.Integer()
+    username = fields.String(required=True)
+    password = fields.String(required=True)
+
+    @validates("username")
+    def username_validations(self, value):       
+        user = User.select().where(User.username==value)
+        if len(user)==0:
+            raise ValidationError("Username is not registered")    
+
 
 user_schema = UserSerializer()
 users_schema = UserSerializer(many=True)
-
+login_schema = LoginSerializer()
 note_schema = NoteSerializer()
 notes_schema = NoteSerializer(many=True)
--- src/server.py
+++ src/server.py
@@ -1,11 +1,32 @@
-from serializers import user_schema, user_schema, note_schema, notes_schema
+from truckpad.bottle.cors import enable_cors
+from peewee import DoesNotExist
+from serializers import users_schema, user_schema, note_schema, notes_schema, login_schema
 from bottle import run, Bottle, response, request
 from models import User, Note
-from truckpad.bottle.cors import enable_cors
 import jwt
 
-
 app = Bottle()
+
+##############################################
+#                                            #
+#               Aux functions                #
+#                                            #
+##############################################
+
+def token_auth(auth_token):
+    try:
+        decoded = jwt.decode(auth_token, "TOY_SECRET_KEY")['id']
+        return User.get(User.id==decoded)
+    except (jwt.DecodeError, DoesNotExist):
+        return None
+
+
+##############################################
+#                                            #
+#                   Users                    #
+#                                            #
+##############################################
+
 
 @enable_cors
 @app.route('/signup', method=['POST'])
@@ -19,5 +40,68 @@ def signup():
         password = data.data['password']
         User.create(username = username, password = password)
         return {"result": "success", "status": response.status_code}
-    
+
+@enable_cors
+@app.route('/login', method=['POST'])
+def login():
+    data = login_schema.load(request.json)
+    if data.errors:
+        response.status= 405
+        return {"result": data.errors, "status": response.status_code}
+    else:
+        user = User.get(User.username == data.data['username'])
+        real_password = user.password    
+        user_id = user.id
+        if data.data['password'] != real_password:
+            response.status= 401
+            return {"result": {"password": ["Wrong password"]}, "status": response.status_code}    
+        else:
+            token = jwt.encode({"username": data.data['username'], "id": user_id},"TOY_SECRET_KEY", algorithm="HS256")
+            return {"result": {"token":token}, "status": response.status_code}    
+        
+        
+@enable_cors
+@app.route('/users', method=['GET'])
+def all_users():
+    users = [user['username'] for user in User.select().dicts()]
+    return {"result": "success", "status": response.status_code, "users": users}
+
+##############################################
+#                                            #
+#                  Notes                     #
+#                                            #
+##############################################
+
+@enable_cors
+@app.route('/notes', method=['POST'])
+def add_note():
+    auth_user = token_auth(request.headers.get('Authorization'))
+    if auth_user is None:
+        response.status= 401
+        return {"result": {"token": ["Wrong token"]}, "status": response.status_code}        
+    else:
+        data = note_schema.load(request.json)
+        if data.errors:
+            response.status= 405
+            return {"result": data.errors, "status": response.status_code}
+        else:
+            title = data.data['title']
+            Note.create(title = title, user = auth_user)
+            return {"result": "success", "status": response.status_code}        
+
+
+@enable_cors
+@app.route('/notes', method=['GET'])
+def add_note():
+    auth_user = token_auth(request.headers.get('Authorization'))
+    if auth_user is None:
+        response.status= 401
+        return {"result": {"token": ["Wrong token"]}, "status": response.status_code}        
+    else:
+        selected_notes = Note.select().where(Note.user==auth_user).dicts()
+        notes = [{"title": note['title'], "creation_date": str(note['creation_date'])} for note in selected_notes]
+        return {"result": "success", "status": response.status_code, "notes": notes}  
+
+
+
 run(app, host='localhost', port=8000)
